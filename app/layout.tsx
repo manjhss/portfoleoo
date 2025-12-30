4948e9002c3a06cfc9b565ae92252a62317563c9
Surendra Manjhi <manjhisurendra08@gmail.com>
2025-12-30 21:22:14 +0530

    refactor: remove unnecessary whitespace in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={FuzzyBubbles.variable} suppressHydrationWarning>
       <body>
-        {" "}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
