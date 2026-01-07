d3fdd1a9d57c3b93c2005c661b04884344244b73
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-01-07 23:08:34 +0530

    refactor: update text color in contact section for improved readability

--- components/portfolio/contact-section.tsx
+++ components/portfolio/contact-section.tsx
@@ -8,7 +8,7 @@ export function ContactSection() {
 
   return (
     <div>
-      <p className="text-sm text-orange-500 mb-6 font-medium">
+      <p className="text-sm text-primary mb-6 font-medium">
         need a committed developer to build or improve your
         product, <br className="hidden sm:block" />
         feel free to connect -
