5ec7ec3ecb1eec56c72c20149880f981cf7bfde2
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-01-10 18:28:03 +0530

    feat: add Open Graph and Twitter metadata for improved sharing; include open-graph-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,27 @@ const RedHatMono = Red_Hat_Mono({
 export const metadata: Metadata = {
   title: "Surendra Manjhi | @manjhss",
   description: portfolioData.profile.bio,
+  openGraph: {
+    title: "Surendra Manjhi | @manjhss",
+    description: portfolioData.profile.bio,
+    images: [
+      {
+        url: "/open-graph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Surendra Manjhi's Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Surendra Manjhi | @manjhss",
+    description: portfolioData.profile.bio,
+    images: ["/open-graph-image.png"],
+    creator: "@manjhss",
+  },
 };
 
 export default function RootLayout({
